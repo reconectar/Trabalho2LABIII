87e273c434689d1e9921690ca23dc3e131c70a14
reconectar <arthurgbranco2@gmail.com>
2020-04-18 22:00:48 -0300

    Added nanoseconds to timer

--- src/util/Timer.java
+++ src/util/Timer.java
@@ -1,20 +1,30 @@
 /* @author "Arthur Gramiscelli Branco"
    Created on 24/05/2019
+   Updated on 18/04/2020
+   This class gets the system time on instantiation and it's methods
+   can be used to calculate final time
 */
 
 package util;
 
 public class Timer {
 
-	private long startingTime;
+	private long startingTimeMili;
+	private long startingTimeNano;
 	private long totalTime;
 
 	public Timer() {
-		this.startingTime = System.currentTimeMillis();
+		this.startingTimeMili = System.currentTimeMillis();
+		this.startingTimeNano = System.nanoTime();
+	}
+
+	public long getTotalTimeInNano() {
+		this.totalTime = System.nanoTime() - startingTimeNano;
+		return totalTime;
 	}
 
 	public long getTotalTimeInMillis() {
-		this.totalTime = System.currentTimeMillis() - startingTime;
+		this.totalTime = System.currentTimeMillis() - startingTimeMili;
 		return totalTime;
 	}
 
